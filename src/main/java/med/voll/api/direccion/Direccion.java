dfd72929ae7e1084d9d744cbafab976588166c24
Paco Cantu <pf.cantuvillanueva@outlook.com>
2025-01-14 23:14:58 -0600

    primer commit del proyecto API con spring boot de ONE

---
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -0,0 +1,27 @@
+package med.voll.api.direccion;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+@AllArgsConstructor
+public class Direccion {
+
+    private String calle;
+    private String numero;
+    private String complemento;
+    private String distrito;
+    private String ciudad;
+
+    public Direccion(){}
+
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.numero = direccion.numero();
+        this.distrito = direccion.distrito();
+        this.complemento = direccion.complemento();
+        this.ciudad = direccion.ciudad();
+    }
+}
